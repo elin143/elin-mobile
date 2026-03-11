f4b53f87eaa95b94a13fda04433150d8f473b7ee
Pertemuan2 <faras24si@mahasiswa.pcr.ac.id>
2026-03-11 15:56:27 +0700

    first commit

---
+++ app/src/main/java/com/example/seonghyeonapps/pertermuan_2/BelajarKotlin.kt
@@ -0,0 +1,45 @@
+package com.example.seonghyeonapps.pertermuan_2
+
+fun main(){
+    println("hai rekan rekan...")
+    println("selamat datang di pemrograman kotlin")
+
+    println("=============")
+
+    val angka = 15
+    println("hasil dari 15+10 =${angka + 10}")
+
+    val nilaiInt = 10000
+    val nilaiDouble = 100.003
+    val nilaiFloat = 1000.0f
+
+    println("nilai integer = $nilaiInt")
+    println("nilai double = $nilaiDouble")
+    println("nilai Float = $nilaiFloat")
+
+    println("===========STRING============")
+    val huruf = 'a'
+    println("ini penggunaaan karakter ' $huruf'")
+
+    val nilaiString = "mawar"
+    println("halo $nilaiString!\nApa kabar?")
+
+    println("==========kondisi==========")
+
+    val nilai = 10
+    if(nilai<0)
+        println("bilangan negatif")
+    else{
+        if (nilai%2 == 0)
+            println("bilangan genap")
+        else
+            println("bilangan ganjil")
+    }
+
+    println("=========perulangan==========")
+    val kampusKu : Array<String> = arrayOf ("kampus","politeknik","caltex","riau")
+    for (kampus : String in kampusKu) {
+        println("kampus")
+    }
+}
+
