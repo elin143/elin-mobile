7d884528c7c7acde679cc7638185b3915d0d7100
elin134 <eveline24si@mahasiswa.pcr.ac.id>
2026-04-29 16:49:58 +0700

    pertemuan 7. fragment home

--- app/src/main/java/Home/HomeFragment.kt
+++ app/src/main/java/Home/HomeFragment.kt
@@ -1,18 +1,81 @@
 package Home
 
+import Home.pertemuan3.ThirdActivity
+import Home.pertemuan4.FourthActivity
+import Home.pertemuan_7.SeventhActivity
+import Home.pertermuan_2.SecondActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.seonghyeonapps.AuthActivity
 import com.example.seonghyeonapps.R
+import com.example.seonghyeonapps.databinding.FragmentHomeBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import pertemuan_5.activity_fifth
+
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sharedPref = requireActivity().getSharedPreferences("user_pref", AppCompatActivity.MODE_PRIVATE)
+
+        binding.btn2.setOnClickListener {
+            val intent = Intent(requireContext(), SecondActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btn3.setOnClickListener {
+            val intent = Intent(requireContext(), ThirdActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btn4.setOnClickListener {
+            val intent = Intent(requireContext(), FourthActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btn5.setOnClickListener {
+            val intent = Intent(requireContext(), activity_fifth::class.java)
+            startActivity(intent)
+        }
+        binding.btn7.setOnClickListener {
+            val intent = Intent(requireContext(), SeventhActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnlogout.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin logout?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+
+                    val editor = sharedPref.edit()
+                    editor.clear()
+                    editor.apply()
+
+                    val intent = Intent(requireContext()    , AuthActivity::class.java)
+
+                    requireActivity().finish()
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
+        }
     }
 }
--- app/src/main/java/com/example/seonghyeonapps/BaseActivity.kt
+++ app/src/main/java/com/example/seonghyeonapps/BaseActivity.kt
@@ -1,11 +1,15 @@
 package com.example.seonghyeonapps
 
+import Home.HomeFragment
+import Message.MessageFragment
+import More.MoreFragment
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.seonghyeonapps.databinding.ActivityBaseBinding
 
 class BaseActivity : AppCompatActivity() {
@@ -15,28 +19,39 @@ class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_base)
+        binding = ActivityBaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+
+        replaceFragment(HomeFragment())
         binding.bottomNavView.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.home -> {
-                    Toast.makeText(this, "Home Clicked", Toast.LENGTH_SHORT).show()
+                    replaceFragment(HomeFragment())
                     true
                 }
                 R.id.message -> {
-                    Toast.makeText(this, "Message Clicked", Toast.LENGTH_SHORT).show()
+                    replaceFragment(MessageFragment())
                     true
                 }
                 R.id.more -> {
-                    Toast.makeText(this, "More Clicked", Toast.LENGTH_SHORT).show()
+                    replaceFragment(MoreFragment())
                     true
                 }
                 else -> false // return false jika item tidak ada yang di klik
             }
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, fragment)
+            //.addToBackStack(null) -> ini kita nonaktifkan agar saat back langsung keluar aplikasi
+            .commit()
     }
 }
