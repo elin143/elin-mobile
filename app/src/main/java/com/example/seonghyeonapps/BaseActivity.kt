967595b211a42ff288fd5e78ceb2a90bef3e6e5f
elin134 <eveline24si@mahasiswa.pcr.ac.id>
2026-04-29 16:15:08 +0700

    pertemuan 7. fragment

--- app/src/main/java/com/example/seonghyeonapps/BaseActivity.kt
+++ app/src/main/java/com/example/seonghyeonapps/BaseActivity.kt
@@ -1,12 +1,17 @@
 package com.example.seonghyeonapps
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.seonghyeonapps.databinding.ActivityBaseBinding
 
 class BaseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBaseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,23 @@ class BaseActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.bottomNavView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    Toast.makeText(this, "Home Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.message -> {
+                    Toast.makeText(this, "Message Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.more -> {
+                    Toast.makeText(this, "More Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false // return false jika item tidak ada yang di klik
+            }
         }
     }
 }
